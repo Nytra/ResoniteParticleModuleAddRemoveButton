a4ae3dd3d76f62fa157c39479961d314a23db47d
Nytra <14206961+Nytra@users.noreply.github.com>
2024-12-17 19:20:55 +0000

    Initial commit

---
+++ ParticleModuleAddRemoveButton/BasicPatcher.cs
@@ -0,0 +1,70 @@
+﻿using FrooxEngine;
+using MonkeyLoader.Resonite.UI;
+using FrooxEngine.PhotonDust;
+using FrooxEngine.UIX;
+
+namespace ParticleModuleAddRemoveButton
+{
+    internal class ParticleSystemModuleInspectorHandler : ResoniteInspectorMonkey<ParticleSystemModuleInspectorHandler, BuildInspectorBodyEvent>
+    {
+        public ParticleSystemModuleInspectorHandler() : base(typeof(ParticleSystemModule<>))
+        { }
+
+        public override int Priority => HarmonyLib.Priority.First;
+        protected override void Handle(BuildInspectorBodyEvent eventData)
+        {
+            ParticleInspectorHelper.Handle(eventData);
+        }
+    }
+
+    internal class ParticleSystemRendererModuleInspectorHandler : ResoniteInspectorMonkey<ParticleSystemRendererModuleInspectorHandler, BuildInspectorBodyEvent>
+    {
+        public ParticleSystemRendererModuleInspectorHandler() : base(typeof(ParticleSystemRendererModule<,>))
+        { }
+
+        public override int Priority => HarmonyLib.Priority.First;
+        protected override void Handle(BuildInspectorBodyEvent eventData)
+        {
+            ParticleInspectorHelper.Handle(eventData);
+        }
+    }
+
+    internal static class ParticleInspectorHelper
+    {
+        public static void Handle(BuildInspectorBodyEvent eventData)
+        {
+            var particleStyle = eventData.Worker.FindNearestParent<Slot>().GetComponentInParents<ParticleStyle>();
+            string text = "No ParticleStyle Found";
+            if (particleStyle != null)
+            {
+                if (particleStyle.Modules.Contains((IParticleSystemSubsystem)eventData.Worker))
+                {
+                    text = "Remove Module";
+                }
+                else
+                {
+                    text = "Add Module";
+                }
+            }
+            eventData.UI.LocalActionButton(text, (Button btn) =>
+            {
+                var particleStyle = eventData.Worker.FindNearestParent<Slot>().GetComponentInParents<ParticleStyle>();
+                string text = "No ParticleStyle Found";
+                if (particleStyle != null)
+                {
+                    if (particleStyle.Modules.Contains((IParticleSystemSubsystem)eventData.Worker))
+                    {
+                        particleStyle.Modules.Remove((IParticleSystemSubsystem)eventData.Worker);
+                        text = "Add Module";
+                    }
+                    else
+                    {
+                        particleStyle.Modules.Add((IParticleSystemSubsystem)eventData.Worker);
+                        text = "Remove Module";
+                    }
+                }
+                btn.LabelText = text;
+            });
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+Distant Component Optimizer
+=============================
+
+Limits the update frequency of components that are too far away from you. Configurable.
+
+## Installation
+
+1. Install [MonkeyLoader Resonite GamePack](https://github.com/ResoniteModdingGroup/MonkeyLoader.GamePacks.Resonite)
+2. [Download the mod](https://github.com/Nytra/ResoniteDistantComponentOptimizer/releases/latest)
+3. Place it in your `MonkeyLoader/Mods` folder inside your Resonite installation
